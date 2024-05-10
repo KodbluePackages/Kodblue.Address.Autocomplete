a39983af0efce1902902030a37bbe24837193344
Mehmet <36473707+ustadstar@users.noreply.github.com>
2024-05-10 12:22:50 +0200

    #7 initial commit & beta release

---
+++ Kodblue.Autocomplete.Address/AutocompleteAddressComposer.cs
@@ -0,0 +1,16 @@
+﻿using Kodblue.Autocomplete.Address.Fields;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Forms.Core.Providers;
+
+namespace Kodblue.Autocomplete.Address
+{
+    public class AutocompleteAddressComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.WithCollectionBuilder<FieldCollectionBuilder>()
+                .Add<AutocompleteAddress>();
+        }
+    }
+}
---
+++ Kodblue.Autocomplete.Address/Fields/AutocompleteAddress.cs
@@ -0,0 +1,25 @@
+﻿using Umbraco.Forms.Core;
+using Umbraco.Forms.Core.Enums;
+
+namespace Kodblue.Autocomplete.Address.Fields
+{
+    public class AutocompleteAddress : FieldType
+    {
+        public AutocompleteAddress()
+        {
+            Id = new Guid("f3b3b3b3-3b3b-3b3b-3b3b-3b3b3b2b3b3b");
+            Name = "Address Autocomplete";
+            Description = "Address Autocomplete";
+            Icon = "icon-location";
+            DataType = FieldDataType.String;
+            SortOrder = 10;
+            SupportsPreValues = false;
+            SupportsRegex = false;
+            MandatoryByDefault = true;
+
+            FieldTypeViewName = "FieldType.AutocompleteAddress.cshtml";
+        }
+
+        public override string GetDesignView() => "~/App_Plugins/Kodblue.Autocomplete.Address/Backoffice/FieldTypes/AutocompleteAddress.html";
+    }
+}
